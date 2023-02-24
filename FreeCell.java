9232d4ceccc82b0b8af8a072f9a50baa6acb6793
VanGriffith <vc13grif@siena.edu>
2023-02-23 19:31:05 -0500

    Add Action Tracking, modify print statement

--- FreeCell.java
+++ FreeCell.java
@@ -22,53 +22,28 @@ public class FreeCell
 
         } catch (FileNotFoundException e) {
             System.err.println("File Not Found, using random gamestate");
-
         }
+        
         game.display();
+        System.out.println("\nSolving...\n");
         
         try {
-            AStarSolve(game);
+            Action.dumpToFile(solve(game), "actions.txt");
         } catch (OutOfMemoryError e) {
-            System.err.println("Out of Memory");
+            System.err.println("Out of Memory :(");
         }
-
-        /* 
-        ArrayList<Action> moves = game.getLegalActions();
-        for (Action a : moves) {
-            System.out.println(a.toDisplayString());
-        }
-        Action a1 = moves.get(moves.size()-2);
-        GameState g2 = game.nextState(a1);
-        System.out.println();
-        g2.display();
-        moves = g2.getLegalActions();
-        for (Action a : moves) {
-            System.out.println(a.toDisplayString());
-        }
-        Action a2 = moves.get(moves.size()-3);
-        GameState g3 = g2.nextState(a2);
-        System.out.println();
-        g3.display();
-        moves = g3.getLegalActions();
-        for (Action a : moves) {
-            System.out.println(a.toDisplayString());
-        }
-        */
     }
 
-    public ArrayList<Action> solve(GameState gs) {
-        return null;
-    }
-
-
-    private static ArrayList<Action> AStarSolve(GameState start) {
+    public static ArrayList<Action> solve(GameState start) {
         PriorityQueue<GameState> pQueue = new PriorityQueue<GameState>();
         pQueue.offer(start);
+        
         while (!pQueue.isEmpty()) {
             GameState gs = pQueue.poll();
             if (gs.isWin()) {
-                System.out.printf("You won in %d moves!%n", gs.getNumSteps());
-                return null;
+                ArrayList<Action> actions = gs.returnedActions;
+                System.out.printf("You won in %d moves!%n", actions.size());
+                return actions;
             }
             else {
                 ArrayList<Action> actions = gs.getLegalActions();
